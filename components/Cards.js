283206578ed24d13d63ee4c877de687e1c949c47
unknown <jackcharp69@gmail.com>
2020-08-16 20:29:33 -0400

    making strides on cards section

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,45 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+//axios call to get data structure
+axios.get('https://lambda-times-api.herokuapp.com/articles')
+.then(res => {
+    console.log(res)
+})
+.catch(err => {
+    console.log(err)
+})
+
+//creating each element and addin clases and content as i go
+function createCard(obj){
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    const headline = documetn.createElement('div');
+    headline.classList.add('headline');
+    headline.innerText = obj.headline;
+
+    const author = document.createElement('hiv');
+    author.classList.add('author');
+    
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+
+    const img = document.createElement('img');
+    img.src = obj.authorPhoto;
+
+    const span = document.createElement('span');
+    span.innerText = obj.authorName;
+
+    //setting up the structure
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    card.appendChild(span);
+
+    return card;
+
+}
+
