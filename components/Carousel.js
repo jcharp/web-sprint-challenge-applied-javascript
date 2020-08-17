382757875920641363866d58b9037551bd7aa11b
unknown <jackcharp69@gmail.com>
2020-08-16 21:51:49 -0400

    attempted stretch.  did not get it to work

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,47 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createCarousel() {
+  const carousel = document.createElement('div');
+  carousel.classList.add('carousel');
+
+  const leftButton = document.createElement('div');
+  leftButton.classList.add('left-button')
+  leftButton.innerText = "<";
+  // leftButton.addEventListener('click', () {
+ 
+    
+  // });
+
+  const mountains = document.createElement('img');
+  mountains.src = "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg";
+
+  const computer = document.createElement('img');
+  computer.src = "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg";
+
+  const trees = document.createElement('img');
+  trees.src = "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg";
+ 
+  const turntable = document.createElement('img');
+  turntable.src = "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg";
+
+  const rightButton = document.createElement('div');
+  rightButton.classList.add('right-button');
+  rightButton.innerText = ">";
+
+  carousel.appendChild(leftButton);
+  carousel.appendChild(mountains);
+  carousel.appendChild(computer);
+  carousel.appendChild(trees);
+  carousel.appendChild(turntable);
+  carousel.appendChild(rightButton);
+  
+  return carousel;
+
+}
+
+const carouselContainer = document.querySelector('.carousel-container');
+
+carouselContainer.appendChild(createCarousel());
+
